c4f6fe6d156f4bb84e91816fa79154e9f196c203
Carlos Barbero <carlos.barbero@live.com>
2021-03-08 19:34:41 +0000

    Added basic structure for interface class and method to find joystick position

--- Core/Interface.cpp
+++ Core/Interface.cpp
@@ -1,11 +1,39 @@
 #include "Interface.h"
 
-Person::Interface()
+#include "Arduino.h"
+
+Interface::Interface(int Rx, int Ry)
+{
+    //Pins definitions
+    this->Rx = Rx;
+    this->Ry = Ry;
+    //Set pin modes
+    pinMode(Rx,INPUT);
+    pinMode(Ry,INPUT);
+    
+    this->xSpeed = 0;//Speeds of movement for joystick
+    this->ySpeed = 0;
+}
+
+Interface::~Interface()
 {
 
 }
+JoystickPos Interface::getJoyPos()
+{
+    int x = (analogRead(this->Rx)*9/1024)-4;//values of x and y axis can take the values -4,-3,-2,-1,0,1,2,3,4. 
+    int y = (analogRead(this->Ry)*9/1024)-4;//Having 9 levels of precision means having clearly defined resting state and possibility for speeds
+    JoystickPos values(x,y);
+    return values;
+}
 
-Person::~Interface()
+
+JoystickPos::JoystickPos(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+JoystickPos::~JoystickPos()
 {
 
 }
--- Core/Interface.h
+++ Core/Interface.h
@@ -1,9 +1,31 @@
-class Interface
+#include "Arduino.h"
+
+class JoystickPos
+/*Type to store x and y position of joystic*/
 {
 private:
-    //
+    
+public:
+    int x;
+    int y;
+    JoystickPos(int,int);
+    ~JoystickPos();
+};
+
+class Interface
+/*Main class that is used to make a interface object which will control all input and output with the user*/
+{
+private:
+    //pins
+    int Rx;
+    int Ry;
+
+    int xSpeed;
+    int ySpeed;
 
 public:
-    Interface();    //Constructor prototype
+    Interface(int, int);    //Constructor prototype 
     ~Interface();   //Destructor prototype
+
+    JoystickPos getJoyPos();
 };
